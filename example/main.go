59d546791096e414f0c651aa2a8e6e060b03fea3
rluisr <detteiu.x14th@gmail.com>
2019-06-13 21:32:02 +0900

    :)

---
+++ example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"deepAI"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	c := deepAI.Client{
+		Token: os.Getenv("DEEPAI_API"),
+	}
+
+	fmt.Println("DetectWithURL")
+	nudityResponse, err := c.DetectWithURL("https://img2.thejournal.ie/inline/2442982/original/?width=516&version=2442982")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n\n", nudityResponse)
+
+	fmt.Println("DetectWithFile")
+	data, err := ioutil.ReadFile("test.jpg")
+	if err != nil {
+		panic(err)
+	}
+	nudityResponse, err = c.DetectWithFile(data, "test.jpg")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", nudityResponse)
+}
